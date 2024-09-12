0d01160c0f97913762c722f9ad7932cbcf7f1b80
Jady Lima <jadylsilva@gmail.com>
2024-09-12 07:56:34 -0300

    Update todos_local_storage_service.dart

--- todo_list_provider/lib/shared/services/local_storage/todos_local_storage_service.dart
+++ todo_list_provider/lib/shared/services/local_storage/todos_local_storage_service.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
 import 'dart:developer';
 
-import 'package:todo_list_provider/features/todos/screens/todos_screen.dart';
 import 'package:todo_list_provider/shared/consts/message_const.dart';
 import 'package:todo_list_provider/shared/errors/local_storage_exceptions.dart';
 import 'package:todo_list_provider/shared/models/todo_model.dart';
 import 'package:todo_list_provider/shared/services/local_storage/local_storage_service.dart';
 
 class TodosLocalStorageService {
-  final String todosKey = "todos";
+  final String todosKey = "todosKey";
+  final String doneTodosKey = "doneTodosKey";
   final LocalStorageService _localStorageService;
 
   TodosLocalStorageService(
@@ -48,6 +48,42 @@ class TodosLocalStorageService {
       return ("Erro loading task", null);
     } catch (error, st) {
       log("Error loading todos", error: error, stackTrace: st);
+      return (defaultErrorMassage, null);
+    }
+  }
+
+  Future<String?> setDoneTodos(List<String> doneTodos) async {
+    try {
+      final String data = jsonEncode(doneTodos);
+
+      await _localStorageService.set(doneTodosKey, data);
+
+      return null;
+    } on LocalStorageException {
+      return "Erro saving done task";
+    } catch (error, st) {
+      log("Error saving done todos", error: error, stackTrace: st);
+      return defaultErrorMassage;
+    }
+  }
+
+  Future<(String? error, List<String>? doneTodos)> getDoneTodos() async{
+    try {
+      final String? doneTodosJson = await _localStorageService.get(doneTodosKey);
+
+      if(doneTodosJson != null) {
+        final doneTodos = (jsonDecode(doneTodosJson) as List)
+                      .cast<String>();
+
+        return (null, doneTodos);
+      }
+
+      return (null, <String>[]);
+
+    } on LocalStorageException {
+      return ("Erro loading done task", null);
+    } catch (error, st) {
+      log("Error loading done todos", error: error, stackTrace: st);
       return (defaultErrorMassage, null);
     }
   }
